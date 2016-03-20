f3b1460514a3c5fb5a0cdc0a0ea2b825c4f91919
jason111neo <jason111neo@hotmail.com>
2016-03-20 10:05:33 +0800

    MovieReview- Top Grossing added (IMDB Deadpool is too much)

---
+++ XinemaActual/App_Data/RT.cs
@@ -0,0 +1,11 @@
+﻿namespace XinemaActual.App_Data
+{
+}
+
+namespace XinemaActual.App_Data
+{
+}
+
+partial class NewDataSet
+{
+}
--- XinemaActual/Controllers/MovieReviewController.cs
+++ XinemaActual/Controllers/MovieReviewController.cs
@@ -7,6 +7,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 using XinemaActual.DAL;
 using System.Web.Mvc;
 using System.Web.Helpers;
+using System.Data;
 
 namespace XinemaActual.Controllers
 {
@@ -23,27 +24,59 @@ namespace XinemaActual.Controllers
         }
         public ActionResult Index()
         {
+
+            var IMDBSet = new DataSet();
+            IMDBSet.ReadXmlSchema(Server.MapPath("~/App_Data/IMDB.xsd"));
+            IMDBSet.ReadXml(Server.MapPath("~/App_Data/IMDB.xml"));
+            var IMDBView = new DataView(IMDBSet.Tables[0]);
+            var IMDBChart = new Chart(width: 600, height: 400)
+                .AddTitle("Top Grossing IMDB Box Office")
+                .AddSeries("Default", chartType: "Pie",
+                    xValue: IMDBView, xField: "MovieName",
+                    yValues: IMDBView, yFields: "MovieReview");
+            IMDBChart.Save("~/Content/IMDBChart.jpg", "jpeg");
+
+            var RTSet = new DataSet();
+            RTSet.ReadXmlSchema(Server.MapPath("~/App_Data/RT.xsd"));
+            RTSet.ReadXml(Server.MapPath("~/App_Data/RT.xml"));
+            var RTView = new DataView(RTSet.Tables[0]);
+            var pieChart = new Chart(width: 600, height: 400)
+                .AddTitle("Top Grossing Rotten Tomatoes Box Office")
+                .AddSeries("Default", chartType: "Pie",
+                    xValue: RTView, xField: "MovieName",
+                    yValues: RTView, yFields: "MovieReview");
+            pieChart.Save("~/Content/RTChart.jpg", "jpeg");
+
             return View(movieReviewGateway.SelectAll());
 
         }
         public ActionResult Details(int? id)
         {
 
-            var chart = new Chart(width: 600, height: 400)
+            var barChart = new Chart(width: 600, height: 400)
              .AddTitle("Movie Review Rating for " + movieReviewGateway.SelectById(id).movieReviewName)
              .AddSeries(
                  name: "Movie",
                  xValue: new[] { "IMDB", "Rotten Tomatoes","Overall" },
                  yValues: new[] { movieReviewGateway.SelectById(id).movieReviewIMDB, movieReviewGateway.SelectById(id).movieReviewRottenTomato,
                ((double.Parse(movieReviewGateway.SelectById(id).movieReviewIMDB) + double.Parse(movieReviewGateway.SelectById(id).movieReviewRottenTomato))/2).ToString() });
-            chart.Save("~/Content/chart.jpg", "jpeg");
+            barChart.Save("~/Content/barChart.jpg", "jpeg");
             // Return the contents of the Stream to the client
             return View(movieReviewGateway.SelectById(id));
         }
-        public ActionResult chart()
+        public ActionResult barChart()
         {
-            
-            return base.File("~/Content/chart.jpg","jpeg");
+            return base.File("~/Content/barChart.jpg","jpeg");
+
+        }
+        public ActionResult IMDBChart()
+        {
+            return base.File("~/Content/IMDBChart.jpg", "jpeg");
+
+        }
+        public ActionResult RTChart()
+        {
+            return base.File("~/Content/RTChart.jpg", "jpeg");
 
         }
 
